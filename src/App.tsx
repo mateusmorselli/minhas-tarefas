094da39a76ff2d62a63aeb09abe20d34d3c6ff33
Mateus Morselli <matmorselli@gmail.com>
2025-03-17 20:29:00 -0300

    instalação do react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
-import BarraLateral from './components/BarraLateral'
-import ListaDeTarefas from './components/ListaDeTarefas'
-import EstiloGlobal, { Container } from './styles'
 import { Provider } from 'react-redux'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import EstiloGlobal, { Container } from './styles'
 
 import store from './store'
+import Home from './pages/Home'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/novo',
+    element: <h1>Nova tarefa</h1>
+  }
+])
 
 function App() {
   return (
     <Provider store={store}>
       <EstiloGlobal />
       <Container>
-        <BarraLateral />
-        <ListaDeTarefas />
+        <RouterProvider router={rotas} />
       </Container>
     </Provider>
   )
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import BarraLateral from '../../components/BarraLateral'
+import ListaDeTarefas from '../../components/ListaDeTarefas'
+
+const Home = () => (
+  <>
+    <BarraLateral />
+    <ListaDeTarefas />
+  </>
+)
+
+export default Home
